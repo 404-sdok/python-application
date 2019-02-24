b66b9b35839d4aab7903b0082cdc8b41e8151b59
youngprogram <43106992+youngprogram@users.noreply.github.com>
2019-02-24 21:27:14 +0900

    Update app.py

--- younghyun/app.py
+++ younghyun/app.py
@@ -1,1 +1,21 @@
+account = {
+          'naver' : {
+            'id': 'naver id',
+            'pw' : 'naver pw'
+          },
+           'facebook' : {
+           'id' : 'facebook id',
+           'pw' : 'facebook pw'
+           },
+          'kakao' : {
+          'id' : 'kakao id',
+          'pw' : 'kakao pw'
+          }
+        }
+       
 
+wihle(1):
+      print('naver,facebook,kakao?',end='')
+     result = input()
+     if result in ['naver','facebook','kakao']:
+            print ('{} 아이디 : {}, 비밀번호 : {}'. format(result,account[result]['id'],account[result]['pw']))
